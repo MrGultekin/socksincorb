a3b8b7c2bbbe8f73bf34192c5a99cdf87f41d21b
MrGultekin <gultekin.de@gmail.com>
2022-10-05 15:19:28 +0200

    v-binding works with attributes with :

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
 		return {
 			product: 'Socks',
 			description: '100% Cotton Handmade socks.',
-			url: 'http://google.com',
+			url: 'https://google.com/',
 			image: './src/assets/images/socks_green.jpg'
 		}
 	}
