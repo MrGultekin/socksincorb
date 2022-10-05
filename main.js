deb113e426b1d13985b797e985127391cfab2fc6
MrGultekin <gultekin.de@gmail.com>
2022-10-05 20:48:30 +0200

    List rendering done,CSS corrected.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 const app = Vue.createApp({
 	data() {
 		return {
@@ -8,8 +6,15 @@ const app = Vue.createApp({
 			url: 'https://google.com/',
 			image: './src/assets/images/socks_green.jpg',
 			inStock: true,
-			inventory:100,
-			onSale:true,
+			inventory: 100,
+			onSale: true,
+			details: ['50% cotton,30% wool,20% polyester'],
+			variants: [
+				{id: 0o001,color: 'green'},
+				{id: 0o002,color: 'blue'},
+			],
+			sizes:['S','M','L','XL']
+			
 		}
 	}
 })
