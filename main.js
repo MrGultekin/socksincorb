6a86fc737007f07607f36705abf1a824fefbc8ea
MrGultekin <gultekin.de@gmail.com>
2022-10-06 14:38:14 +0200

    simple computed property return this.brand +''+this.product the title

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const app = Vue.createApp({
 		return {
 			cart: 0,
 			product: 'Socks',
+			brand: 'Vue Mastery',
 			description: '100% Cotton Handmade socks.',
 			url: 'https://google.com/',
 			image: './src/assets/images/socks_blue.jpg',
 			inStock: false,
-			inventory:0 ,
-			onSale: false,
+			inventory:10 ,
+			onSale: true,
 			details: ['50% cotton','30% wool','20% polyester'],
 			variants: [
 				{id: 0o001, color: 'green', image:'./src/assets/images/socks_green.jpg'},
@@ -29,6 +30,11 @@ const app = Vue.createApp({
 		},
 		updateImage(variantImage) {
 			this.image = variantImage
+		}
+	},
+	computed: {
+		title(){
+			return this.brand + '' + this.product
 		}
 	}
 })
