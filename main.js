f2ae8f63c1b477562a078bdb7c99e26797d5018a
MrGultekin <gultekin.de@gmail.com>
2022-10-06 09:33:37 +0200

    Detail lists fixed.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const app = Vue.createApp({
 			inStock: true,
 			inventory: 100,
 			onSale: true,
-			details: ['50% cotton,30% wool,20% polyester'],
+			details: ['50% cotton','30% wool','20% polyester'],
 			variants: [
 				{id: 0o001, color: 'green', image:'./src/assets/images/socks_green.jpg'},
 				{id: 0o002, color: 'blue',image:'./src/assets/images/socks_blue.jpg'},
