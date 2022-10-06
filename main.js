080006a50b4340677261e42de228789f35b4027b
MrGultekin <gultekin.de@gmail.com>
2022-10-06 15:57:19 +0200

    advanced computed property works

--- main.js
+++ main.js
@@ -4,16 +4,15 @@ const app = Vue.createApp({
 			cart: 0,
 			product: 'Socks',
 			brand: 'Vue Mastery',
-			description: '100% Cotton Handmade socks.',
+			// description: '100% Cotton Handmade socks.',
 			url: 'https://google.com/',
-			image: './src/assets/images/socks_blue.jpg',
-			inStock: false,
-			inventory:10 ,
-			onSale: true,
-			details: ['50% cotton','30% wool','20% polyester'],
+			selectedVariant:0,
+			// inventory: 10,
+			// onSale: true,
+			details: ['50% cotton', '30% wool', '20% polyester'],
 			variants: [
-				{id: 0o001, color: 'green', image:'./src/assets/images/socks_green.jpg'},
-				{id: 0o002, color: 'blue',image:'./src/assets/images/socks_blue.jpg'},
+				{id: 0o001, color: 'green', image: './src/assets/images/socks_green.jpg', quantity: 50},
+				{id: 0o002, color: 'blue', image: './src/assets/images/socks_blue.jpg', quantity: 0},
 			],
 			sizes: ['S', 'M', 'L', 'XL']
 			
@@ -24,17 +23,24 @@ const app = Vue.createApp({
 			this.cart += 1
 		},
 		removeFromCart() {
-			if (this.cart >=1){
+			if (this.cart >= 1) {
 				this.cart -= 1
 			}
 		},
-		updateImage(variantImage) {
-			this.image = variantImage
+		updateVariant(index) {
+			this.selectedVariant = index;
+			// console.log(index)
 		}
 	},
 	computed: {
-		title(){
+		title() {
 			return this.brand + '' + this.product
+		},
+		image(){
+			return this.variants[this.selectedVariant].image
+		},
+		inStock(){
+			return this.variants[this.selectedVariant].quantity
 		}
 	}
 })
